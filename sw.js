f8bc19c809fddc556c26d2fce752a5eb26ad15da
LivesCreate <yam.benjaminm@gmail.com>
2026-03-02 19:01:40 -0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,47 @@
-var CACHE_NAME = 'flux-v3.4';
-var URLS = [
+var CACHE_NAME = 'flux-v4-1-0b';
+var URLS_TO_CACHE = [
   './',
   './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js',
   'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&family=Playfair+Display:ital,wght@0,400;0,700;0,900;1,400&family=JetBrains+Mono:wght@400;600;700&display=swap'
 ];
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(URLS); }));
-  self.skipWaiting();
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    }).then(function() {
+      return self.skipWaiting();
+    })
+  );
 });
 
-self.addEventListener('activate', function(e) {
-  e.waitUntil(caches.keys().then(function(names) {
-    return Promise.all(names.filter(function(n) { return n !== CACHE_NAME; }).map(function(n) { return caches.delete(n); }));
-  }));
-  self.clients.claim();
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) { return name !== CACHE_NAME; })
+          .map(function(name) { return caches.delete(name); })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    fetch(e.request).then(function(resp) {
-      if (resp && resp.status === 200) {
-        var clone = resp.clone();
-        caches.open(CACHE_NAME).then(function(cache) { cache.put(e.request, clone); });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      if (response && response.status === 200) {
+        var clone = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, clone);
+        });
       }
-      return resp;
+      return response;
     }).catch(function() {
-      return caches.match(e.request).then(function(r) {
-        return r || caches.match('./index.html');
-      });
+      return caches.match(event.request);
     })
   );
 });
